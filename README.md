18e9ef8e06592258707f8f84e054743eee7cf57a
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-06-27 11:09:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Todolist-ts
+
+Todo list for TOP in typescript.
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: "./src/scripts/index.ts",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|gif|)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html",
+            favicon: "./src/assets/favicon.ico",
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        }
+    },
+}
