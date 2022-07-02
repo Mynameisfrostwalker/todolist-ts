a629233ca7e949495fae661503b3b5ca497ad883
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-07-02 17:41:02 +0100

    Added complete property to item object

--- src/scripts/todo-items.ts
+++ src/scripts/todo-items.ts
@@ -1,17 +1,19 @@
 import { correctDateProjects } from "./projects";
 
-type Properties = "title" | "description" | "dueDate" | "priority";
+type Properties = "title" | "description" | "dueDate" | "priority" | "complete";
 
 class Items {
     private title: string;
     private description: string;
     private dueDate: string;
     private priority: string;
-    constructor(title: string, description: string, dueDate: string, priority: string) {
+    private complete: string;
+    constructor(title: string, description: string, dueDate: string, priority: string, complete: string) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.complete = complete;
     }
 
     public getProperty(name: Properties): string {
