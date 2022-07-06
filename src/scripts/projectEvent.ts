24d83e4a8334b783e4ba9338d30fc7b204c608eb
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-07-06 19:37:50 +0100

    Remove add button from date project

--- src/scripts/projectEvent.ts
+++ src/scripts/projectEvent.ts
@@ -3,12 +3,19 @@ import { displayTodo } from "./displayItems";
 
 function changeProject(e: Event) {
     const active = document.querySelector(".active");
+    const addItemButton = document.querySelector(".add-item");
     active?.classList.remove("active");
     if(e.target instanceof HTMLElement) {
         e.target.classList.add("active");
+        if(e.target?.id === "today" || e.target?.id === "thisWeek") {
+            if(!addItemButton?.classList.contains("none")) {
+                addItemButton?.classList.add("none");
+            }
+        }else if(addItemButton?.classList.contains("none")) {
+            addItemButton?.classList.remove("none");
+        }
+        displayTodo();
     }
-
-    displayTodo();
 }
 
 export { changeProject }
