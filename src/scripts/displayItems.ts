f9bae924b24782542af6b3bc0e7f7bfbcb327ebb
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-07-06 16:11:27 +0100

    Alter displayTodo function

--- src/scripts/displayItems.ts
+++ src/scripts/displayItems.ts
@@ -1,7 +1,7 @@
 import { formatDistanceToNow } from "date-fns";
 import { Items } from "./todo-items"
 import { getProject } from "./projects";
-import { deleteForm } from "./todoEvent";
+import { deleteForm, editForm } from "./todoEvent";
 
 function getCurrentProject() {
     const project = document.querySelector(".active");
@@ -37,6 +37,7 @@ function createControls(item: Items) {
     const div1 = document.createElement("div");
     const i1 = document.createElement("i");
     i1.classList.add("fas", "fa-edit");
+    div1.addEventListener("click", editForm, true);
     div1.appendChild(i1);
     alterDiv.appendChild(div1);
 
@@ -52,7 +53,8 @@ function createControls(item: Items) {
     
 }
 
-function displayTodo() {
+/*
+function displaycTodo() {
 
     const projectName = getCurrentProject();
     const main = document.querySelector("main");
@@ -90,5 +92,80 @@ function displayTodo() {
         }
     }
 }
+*/
+
+function displayTodo() {
+
+    const projectName = getCurrentProject();
+    const main = document.querySelector("main");
+    const add = document.querySelector(".add-item");
+    const children = main?.children;
+    let arr: Element[] = [];
+    let arr2: Element[] = [];
+    let projectArr: Items[] = [];
+    if(children) {
+        for (let i = 0; i < children.length; i++) {
+            if(children[i].classList.contains("todo")) {
+                arr.push(children[i])
+            }
+        }
+        // Delete todo item
+        if(projectName) {
+            const project = getProject(projectName);
+            arr = arr.filter((item) => {
+                for (let i = 0; i < project.length; i++) {
+                    if(item.id === project[i].getProperty("id")) {
+                        return false;
+                    }
+                }
+                return true;
+            })
+        }
+        arr.forEach((item) => {
+            item.remove();
+        })
+
+        //Add todo item to screen
+        for (let i = 0; i < children.length; i++) {
+            if(children[i].classList.contains("todo")) {
+                arr2.push(children[i])
+            }
+        }
+
+        if(projectName) {
+            const project = getProject(projectName);
+            projectArr = project.filter((item) => {
+                for (let i = 0; i < arr2.length; i++) {
+                    if(arr2[i].id === item.getProperty("id")) {
+                        return false;
+                    }
+                }
+                return true;
+            })
+        }
+
+    }
+
+    if(projectName) {
+
+        const items = getProject(projectName);
+        for(let i = 0; i < projectArr.length; i++) {
+
+            const item = projectArr[i];
+            const priority = item.getProperty("priority");
+
+            const divItem = document.createElement("div");
+            divItem.classList.add("item");
+            divItem.classList.add(priority);
+            divItem.classList.add("todo");
+            divItem.id = item.getProperty("id");
+            divItem.appendChild(createInfo(item));
+            divItem.appendChild(createControls(item));
+            main?.insertBefore(divItem, add);
+
+        }
+    }
+}
+
 
 export { displayTodo }
--- src/scripts/todoEvent.ts
+++ src/scripts/todoEvent.ts
@@ -9,20 +9,23 @@ function getCurrentProject() {
 
 
 function showForm() {
+    const addDiv = document.querySelector(".add-item");
     const formShowButton = document.querySelector(".form-show");
-    const formContainer = document.querySelector(".form");
+    const formContainer = addDiv?.querySelector(".form");
     formShowButton?.classList.add("none");
     formContainer?.classList.remove("none");
-    const form = document.querySelector("form");
+    const form = formContainer?.querySelector("form");
     form?.addEventListener("submit", submitForm);
 }
 
 function removeForm() {
+    const addDiv = document.querySelector(".add-item");
     const formShowButton = document.querySelector(".form-show");
-    const formContainer = document.querySelector(".form");
+    const formContainer = addDiv?.querySelector(".form");
     formShowButton?.classList.remove("none");
     formContainer?.classList.add("none");
-    const form = document.querySelector("form");
+    const form = formContainer?.querySelector("form");
+    form?.removeEventListener("submit", submitForm);
 }
 
 function submitForm(e: Event) {
@@ -75,8 +78,56 @@ function deleteForm(e: Event) {
         }
     }
 
+}
+
+function submitEditedForm() {
 
 }
 
+function editForm(e: Event) {
+    const element = e.target;
+    let propArr = [];
+    if(element instanceof HTMLElement) {
 
-export { showForm, deleteForm }
+        const main = document.querySelector("main");
+        const projectName = getCurrentProject();
+        const item = element?.parentElement?.parentElement?.parentElement;
+        const id = item?.id;
+        const form = document.querySelector(".form")?.cloneNode(true);
+
+        if(projectName) {
+            const project = getProject(projectName);
+            for(let i = 0; i < project.length; i++) {
+                const item = project[i];
+                if(item.getProperty("id") === id) {
+                    propArr[0] = item.getProperty("title");
+                    propArr[1] = item.getProperty("dueDate");
+                    propArr[2] = item.getProperty("priority");
+                    propArr[3] = item.getProperty("description");
+                    propArr[4] = item.getProperty("complete");
+                    propArr[5] = item.getProperty("id");
+                }
+            }
+            if(form instanceof HTMLElement && item) {
+                form.classList.remove("none");
+                main?.replaceChild(form, item);
+                form.classList.add("todo");
+                const realform = form.querySelector("form");
+                realform?.removeEventListener("submit", submitForm);
+                if(realform) {
+                    for(let i = 0; i < 4; i++) {
+                        const element = realform[i]
+                        if(element instanceof HTMLInputElement || element instanceof HTMLSelectElement || element instanceof HTMLTextAreaElement) {
+                            element.value = propArr[i];
+                        }
+                    }
+                    form.setAttribute("data-complete", propArr[4]);
+                    form.id = propArr[5];
+                }
+            }
+        }
+    }
+}
+
+
+export { showForm, deleteForm, editForm }
